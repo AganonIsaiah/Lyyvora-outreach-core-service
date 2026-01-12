b89507eeeaae8543e9105da87726589c1a57e1bb
AganonIsaiah <isaiahaganon0@gmail.com>
2026-01-11 23:45:32 -0500

    Fix: filter ui width

--- dashboard_ui/app/dashboard/components/table/filters/FilterUI.tsx
+++ dashboard_ui/app/dashboard/components/table/filters/FilterUI.tsx
@@ -96,7 +96,7 @@ export default function FilterUI({
       <p className="text-xs! font-semibold mb-1">{label}</p>
 
       <button
-        className="cursor-pointer text-sm w-33 flex justify-between items-center bg-white border border-gray-200 rounded px-2 py-1 focus:ring-indigo-500 focus:border-indigo-500 transition-all duration-200"
+        className="cursor-pointer text-sm w-36 flex justify-between items-center bg-white border border-gray-200 rounded px-2 py-1 focus:ring-indigo-500 focus:border-indigo-500 transition-all duration-200"
         onClick={() => setIsOpen((v) => !v)}
       >
         <span className="text-gray-400 truncate text-xs font-semibold">
@@ -110,7 +110,7 @@ export default function FilterUI({
       </button>
 
       {isOpen && (
-        <div className="absolute z-20 mt-2 w-33 bg-white border border-gray-300 rounded-lg shadow overflow-x-hidden!">
+        <div className="absolute z-20 mt-2 w-36 bg-white border border-gray-300 rounded-lg shadow overflow-x-hidden!">
           <div className="rounded-t-lg text-xs! font-semibold text-white flex items-center justify-between py-1.5 px-1 border-b border-gray-500">
             <button
               onClick={applyFilters}
