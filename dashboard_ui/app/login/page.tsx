91b269ed2438661246d3143025b84bf2ac227304
AganonIsaiah <isaiahaganon0@gmail.com>
2026-01-19 03:02:45 -0500

    revert

--- dashboard_ui/app/login/page.tsx
+++ dashboard_ui/app/login/page.tsx
@@ -1,31 +1,14 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import LoginCard from "./components/LoginCard";
+
+import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/lib/auth";
-import { useRouter } from "next/navigation";
 
-export default function Login() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true); // show loading until we know user
-  const [user, setUser] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function fetchUser() {
-      const u = await getCurrentUser();
-      console.log("Current user:", u);
-      setUser(u);
-      setLoading(false);
-      if (u) router.replace("/dashboard"); // use replace instead of push to avoid back button issues
-    }
-    fetchUser();
-  }, [router]);
-
-  if (loading) return <div>Loading...</div>; // avoid flashing login form
-
+export default async function Login() {
+  const user = await getCurrentUser();
+  if (user) redirect("/dashboard");
   return (
-    <div className="flex justify-center items-center h-screen">
-      {!user && <LoginCard />}
+    <div className="flex justify-center items-center h-screen!">
+      <LoginCard />
     </div>
   );
 }
